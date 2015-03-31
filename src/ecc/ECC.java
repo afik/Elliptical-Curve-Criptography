f9809cdf69c780fc74c30ad386c2d02bf86679ea
hayyuelha <heripottypot@gmail.com>
2015-03-31 23:28:45 +0700

    tambah GUI

--- src/ecc/ECC.java
+++ src/ecc/ECC.java
@@ -7,6 +7,7 @@
 package ecc;
 
 import com.google.common.math.LongMath;
+import static java.lang.Math.floor;
 import java.util.ArrayList;
 
 /**
@@ -19,7 +20,7 @@ public class ECC {
     //Point pm;
     private String pesan;
     private Point pb;
-    private Curve ec;
+    private Curve ec = new Curve();
     
     //Getter - Setter
     public String getPesan (){
@@ -32,25 +33,44 @@ public class ECC {
     
     //Encode
     public Point encodeChar (long m, long k){
-        Point pm;
+        Point pm = new Point();
         long it = 1;
         long x, y;
-        ec.setEllipticGrup();
-        do {
+        do{
             x = m*k + it;
-            y = 
-        }
+            y = ec.getY(x);
+            if(y != 0){
+                pm.setX(x);
+                pm.setY(y);
+            }
+            it++;
+        }while (ec.getY(x) == 0 && it < k);
+        
         return pm;
+    }
+    
+    //Decode
+    public long decodeChar (long x, long k){
+        long m;
+        m = (x-1) / k;
+        double temp = floor((double)m);
+        return (long)temp;
     }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Curve c = new Curve();
-        long x = 2;
-        System.out.println(c.getY(x));
-        
+//        ec = new Curve();
+//        long x = 2;
+//        System.out.println(c.getY(x));
+        Point pm = new Point();
+        ECC eccrypt = new ECC();
+        pm = eccrypt.encodeChar(11,20);
+        System.out.println(pm.getX() + ", " + pm.getY());
+        long bChar;
+        bChar = eccrypt.decodeChar(224, 20);
+        System.out.println(bChar);
 //        ArrayList<Point> tes = new ArrayList();
 //        tes.add(new Point(2,2));
 //        Point p = new Point(2,2);
