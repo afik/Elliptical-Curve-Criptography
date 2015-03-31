b60a60dd8532fd8f9470f59f120af22042080b55
hayyuelha <heripottypot@gmail.com>
2015-03-31 20:32:53 +0700

    .

--- src/ecc/ECC.java
+++ src/ecc/ECC.java
@@ -10,10 +10,38 @@ import com.google.common.math.LongMath;
 
 /**
  *
- * @author Afik
+ * @author Afik & Hayyu
  */
 public class ECC {
-
+    
+    //Attributes
+    //Point pm;
+    private String pesan;
+    private Point pb;
+    
+    //Getter - Setter
+    public String getPesan (){
+        return pesan;
+    }
+    
+    public void setPesan (String s){
+        this.pesan = s;
+    }
+    
+    //Encode
+    public Point encodeChar (int m, int k){
+        Point pm;
+        int it;
+        int x;
+        for (it=1;it<k;it++){
+            x = m*k + it;
+//            if (){
+//                
+//            }
+        }
+        return pm;
+    }
+    
     /**
      * @param args the command line arguments
      */
